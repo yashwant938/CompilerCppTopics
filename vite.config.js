ca0cdec6d2c86020a0326721e5b234a9a78e07bc
yashwant938 <ranayash2022@gmail.com>
2026-04-14 12:07:14 +0530

    chore: enable trusted local SSL certificates

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  base: '/CompilerCppTopics/'
+  plugins: [react(), mkcert()],
+  base: '/CompilerCppTopics/',
+  server: {
+    https: true
+  }
 })
 
